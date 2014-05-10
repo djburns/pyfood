a35642a7c49fe36a53388f0a84cc60f8c8437546
David Burns <david@burns.cc>
2014-05-10 20:17:03 +0100

    basic models created

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from food.models import Event, Vendor, Order
+
+admin.site.register(Event)
+admin.site.register(Vendor)
+admin.site.register(Order)
 
 # Register your models here.
+
--- food/models.py
+++ food/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    postcode = models.CharField(max_length=8)
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=30)
+
+class Order(models.Model):
+    event = models.ForeignKey(Event)
+    vendor = models.ForeignKey(Vendor)
+    organiser = models.ForeignKey(User)
+    closing_date = models.DateField()
